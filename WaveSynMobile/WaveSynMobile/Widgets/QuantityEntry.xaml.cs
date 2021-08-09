0f18b30b122d7f310c808252b1c30b3f82431580
xialulee <xialulee@live.cn>
2021-08-09 23:10:31 +0800

    Implementing QuantityEntry.

--- WaveSynMobile/WaveSynMobile/Widgets/QuantityEntry.xaml.cs
+++ WaveSynMobile/WaveSynMobile/Widgets/QuantityEntry.xaml.cs
@@ -15,6 +15,19 @@ namespace WaveSynMobile.Widgets
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class QuantityEntry : ContentView
     {
+        public static readonly BindableProperty QuantityTypeProperty = BindableProperty.Create(
+            nameof(QuantityType),
+            typeof(string),
+            typeof(QuantityEntry),
+            default(string),
+            BindingMode.OneWay);
+
+        public string QuantityType
+        {
+            get => (string)GetValue(QuantityTypeProperty);
+            set => SetValue(QuantityTypeProperty, value);
+        }
+
         public static readonly BindableProperty QuantityNameProperty = BindableProperty.Create(
             nameof(QuantityName), 
             typeof(string), 
@@ -27,6 +40,19 @@ namespace WaveSynMobile.Widgets
             get => (string)GetValue(QuantityNameProperty);
             set => SetValue(QuantityNameProperty, value);
         }
+
+        public static readonly BindableProperty QuantityUnitPickerWidthProperty = BindableProperty.Create(
+            nameof(QuantityUnitPickerWidth),
+            typeof(double),
+            typeof(QuantityEntry),
+            default(double),
+            BindingMode.OneWay);
+
+        public double QuantityUnitPickerWidth
+        {
+            get => (double)GetValue(QuantityUnitPickerWidthProperty);
+            set => SetValue(QuantityUnitPickerWidthProperty, value);
+        } 
 
         public static readonly BindableProperty QuantityValueProperty = BindableProperty.Create(
             nameof(QuantityValue),
@@ -59,6 +85,24 @@ namespace WaveSynMobile.Widgets
             {
                 quantityValueEntry.Text = QuantityValue.ToString();
             }
+            else if (propertyName == QuantityTypeProperty.PropertyName)
+            {
+                switch (QuantityType.ToLower())
+                {
+                    case "frequency":
+                        quantityUnitPicker.Items.Add("Hz");
+                        quantityUnitPicker.Items.Add("kHz");
+                        quantityUnitPicker.Items.Add("MHz");
+                        quantityUnitPicker.Items.Add("GHz");
+                        quantityUnitPicker.SelectedIndex = 0;
+                        break;
+                }
+            }
+            else if (propertyName == QuantityUnitPickerWidthProperty.PropertyName)
+            {
+                quantityUnitPicker.WidthRequest = QuantityUnitPickerWidth;
+            }
+            
         }
 
         public QuantityEntry()

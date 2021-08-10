9a8cc017b345871ed672f8cb8926b96aa3802e6c
xialulee <xialulee@live.cn>
2021-08-10 23:07:27 +0800

    Implementing Range resolution equation.

--- WaveSynMobile/WaveSynMobile/Utils/Equations.cs
+++ WaveSynMobile/WaveSynMobile/Utils/Equations.cs
@@ -19,11 +19,33 @@ namespace WaveSynMobile.Utils {
 
             if (frequencyNumber is double numberOfFrequency) {
                 wavelengthNumber = c / (frequencyNumber * units[frequencyUnit]) / units[wavelengthUnit];
-                frequencyNumber = null;
             } else if (periodNumber is double numberOfPeriod) {
                 wavelengthNumber = c * (periodNumber * units[periodUnit]) / units[wavelengthUnit];
             } // else raise Exception
             WavelengthEquation(ref wavelengthNumber, wavelengthUnit, ref frequencyNumber, frequencyUnit, ref periodNumber, periodUnit);
+        }
+
+        public static void RangeResolutionEquation(
+                ref double? rangeResolutionNumber, string rangeResolutionUnit, 
+                ref double? bandwidthNumber, string bandwidthUnit,
+                ref double? chipwidthNumber, string chipwidthUnit) {
+            var c = Constants.lightspeed;
+            var units = PhysicalQuantities.Units;
+
+            if (rangeResolutionNumber is double ΔRnum) {
+                var ΔR = ΔRnum * units[rangeResolutionUnit];
+                var Bw = c / 2 / ΔR;
+                bandwidthNumber = Bw / units[bandwidthUnit];
+                chipwidthNumber = 1 / Bw / units[chipwidthUnit];
+                return;
+            }
+
+            if (bandwidthNumber is double Bwnum) {
+                rangeResolutionNumber = c / 2 / (Bwnum * units[bandwidthUnit]) / units[rangeResolutionUnit];
+            } else if (chipwidthNumber is double Cwnum) {
+                rangeResolutionNumber = c / 2 * (Cwnum * units[chipwidthUnit]) / units[rangeResolutionUnit];
+            }
+            RangeResolutionEquation(ref rangeResolutionNumber, rangeResolutionUnit, ref bandwidthNumber, bandwidthUnit, ref chipwidthNumber, chipwidthUnit);
         }
     }
 }
--- WaveSynMobile/WaveSynMobile/Views/EquationSelectPage.xaml.cs
+++ WaveSynMobile/WaveSynMobile/Views/EquationSelectPage.xaml.cs
@@ -7,20 +7,21 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace WaveSynMobile.Views
-{
+namespace WaveSynMobile.Views {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class EquationSelectPage : ContentPage
-    {
-        public EquationSelectPage()
-        {
+    public partial class EquationSelectPage : ContentPage {
+        public EquationSelectPage() {
             InitializeComponent();
         }
 
-        async private void OnWavelengthEqClicked(object sender, EventArgs e)
-        {
+        async private void OnWavelengthEqClicked(object sender, EventArgs e) {
             var wavelengthEqPage = new Views.WavelengthEquationPage();
             await this.Navigation.PushAsync(wavelengthEqPage);
+        }
+
+        async private void OnRangeResolutionEqClicked(object sender, EventArgs e) {
+            var rangeResolutionEqPage = new Views.RangeResolutionEquationPage();
+            await Navigation.PushAsync(rangeResolutionEqPage);
         }
     }
 }

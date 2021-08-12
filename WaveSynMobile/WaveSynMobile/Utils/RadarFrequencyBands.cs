5dcd16866a659277b2f1f6b38616d92022f22558
xialulee <xialulee@live.cn>
2021-08-12 19:36:36 +0800

    Using Linq instead of for loops.

--- WaveSynMobile/WaveSynMobile/Utils/RadarFrequencyBands.cs
+++ WaveSynMobile/WaveSynMobile/Utils/RadarFrequencyBands.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Linq;
 using System.Globalization;
 using System.Collections.Generic;
 using System.Reflection;
@@ -25,31 +26,21 @@ namespace WaveSynMobile.Utils {
         public RadarFrequencyBands() {
             // See stackoverflow.com/questions/39367349/code-or-command-to-use-embedded-resource-in-visual-studio
             var assembly = Assembly.GetExecutingAssembly();
-            //var allResourceNames = assembly.GetManifestResourceNames();
-
-            _bandInfoList = new List<RadarFrequencyBandInfo>();
-
             using (var stream = assembly.GetManifestResourceStream("WaveSynMobile.Resources.radarfrequencybands.csv"))
             using (var reader = new StreamReader(stream))
             using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture)) {
                 var records = csv.GetRecords<RadarFrequencyBandInfo>();
-                foreach (var record in records) {
-                    _bandInfoList.Add(record);
-                }
+                _bandInfoList = records.ToList();
             }
         }
 
         public string GetName(double frequencyNumber, string frequencyUnit) {
             var units = PhysicalQuantities.UnitsOfFrequency;
             double freqInGHz = frequencyNumber * units[frequencyUnit] / units["GHz"];
-
-            foreach (var bandInfo in _bandInfoList) {
-                if (bandInfo.MinFreqInGHz <= freqInGHz && freqInGHz < bandInfo.MaxFreqInGHz) {
-                    return bandInfo.Name;
-                }
-            }
-
-            return string.Empty;
+            return (from bandInfo in _bandInfoList 
+                    where bandInfo.MinFreqInGHz <= freqInGHz && freqInGHz < bandInfo.MaxFreqInGHz 
+                    select bandInfo.Name)
+                   .FirstOrDefault();
         }
     }
 }
